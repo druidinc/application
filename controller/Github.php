e2de433e5f2af04b16c2d2f868148e861a862e26
e.gutierrez <egeeboygutierrez91@gmail.com>
2013-05-04 00:43:46 +0800

    Github

--- controller/Github.php
+++ controller/Github.php
@@ -4,7 +4,19 @@
 
 	class Github extends Controller{
 		public function main (){
+			exec('git pull https://github.com/Web-Project/jca.git master'); 
+		}
+
+		public function application(){
+			exec('cd application'); 
 			exec('git pull https://github.com/jca-bookkeeping-services/application.git master'); 
+			exec('cd ../');  	
+		}
+
+		public function framework(){
+			exec('cd framework'); 
+			exec('git pull https://github.com/jca-bookkeeping-services/framework.git master');  	
+			exec('cd ../');  	
 		}
 	}
 ?>
