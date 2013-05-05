6d75fcbae174ac371cba5034623f7fac4b40bdd1
egutierrez <egeeboygutierrez91@gmail.com>
2013-05-05 22:30:18 +0800

    modified home

--- view/Home.php
+++ view/Home.php
@@ -35,7 +35,7 @@
     <div class="homeMenu">        
         <div class="menuSide">
             <ul class="ul">
-                <li class="menu_selected"><a href="#">Home</a></li> <!-- just home-->
+                <li class="menu_selected"><a href="#">Home na sad ni</a></li> <!-- just home-->
                 <li><a href="{$base_url}Home/services">Services</a></li>
                 <li><a href="{$base_url}Home/about_us">About Us</a></li>
                 <li><a href="{$base_url}Home/clients">Clients</a></li>
