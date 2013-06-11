7635e9d558a1f3a986f51a44e462184d65103e19
egutierrez <egeeboygutierrez91@gmail.com>
2013-06-11 22:55:35 +0800

    Modified chat

--- view/_shared/_footer.php
+++ view/_shared/_footer.php
@@ -37,4 +37,4 @@
 	<div class = "col span_1_of_2 left">JCA Bookkeeping Services</div>
 	<div class = "col span_1_of_2 right float_right">Powered by <a href="http://www.druidinc.com">DRUID Innovations, Inc.</a></div>
 </div>
-{include file="../_widget/_chat.php"}
+{include file="_widget\_chat.php"}
